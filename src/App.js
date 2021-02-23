96e0851148b3a832bee55b92a776880f9134a98c
DanielMoisei <54266203+DanielMoisei@users.noreply.github.com>
2021-02-23 18:47:09 +0200

    Added projects
    - Added Speed-typing Game
    - Added Manual Currency Converter
    - Added SW API fetch
    - Fixed some conflicting styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import {Link, Switch, Route} from "react-router-dom"
+
+import Homepage from "./Components/Homepage"
+import SpeedtypingGame from "./Components/SpeedtypingGame"
+import CurrencyConverter from "./Components/CurrencyConverter"
+import FetchSwapi from "./Components/FetchSwapi"
+
+
+import "./style.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app-div">
+      <div id="header">
+        <nav>
+          <Link to="/"><h2 className="navlink">Home</h2></Link>
+          <Link to="/speed-game"><h2 className="navlink">Speedtyping Game</h2></Link>
+          <Link to="/curr-converter"><h2 className="navlink">Currency Converter</h2></Link>
+          <Link to="/fetch-swapi"><h2 className="navlink">Star Wars API</h2></Link>
+        </nav>
+      </div>
+
+      <Switch>
+
+        <Route exact path="/">
+          <Homepage />
+        </Route>
+
+        <Route path="/speed-game">
+          <SpeedtypingGame />
+        </Route>
+
+        <Route path="/curr-converter">
+          <CurrencyConverter />
+        </Route>
+
+        <Route path="/fetch-swapi">
+          <FetchSwapi />
+        </Route>
+
+      </Switch>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
